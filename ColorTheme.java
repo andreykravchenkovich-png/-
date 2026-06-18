199bd04e28b4ee13aaf470cca34d9c9b3ce9c33e
andreykravchenkovich-png <andreykravchenkovich@gmail.com>
2026-06-18 08:05:09 +0400

    Create ColorTheme.java

---
+++ ColorTheme.java
@@ -0,0 +1,18 @@
+package com.autodealer.utils;
+
+import java.awt.Color;
+
+public class ColorTheme {
+    public static final Color PRIMARY = new Color(33, 150, 243);
+    public static final Color PRIMARY_DARK = new Color(25, 118, 210);
+    public static final Color SUCCESS = new Color(76, 175, 80);
+    public static final Color WARNING = new Color(255, 193, 7);
+    public static final Color DANGER = new Color(244, 67, 54);
+    public static final Color GRAY = new Color(158, 158, 158);
+    public static final Color WHITE = new Color(255, 255, 255);
+
+    // Фоновые цвета
+    public static final Color BG_START = new Color(240, 248, 255);
+    public static final Color BG_END = new Color(200, 225, 245);
+    public static final Color CARD_BG = new Color(255, 255, 255, 240);
+}
